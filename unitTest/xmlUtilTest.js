9a742f2bfdee448b532637f37c66a8c06abf3391
spursy <582214816@qq.com>
2017-04-05 22:48:42 +0800

    add xmlUtil.js unit test.

--- unitTest/xmlUtilTest.js
+++ unitTest/xmlUtilTest.js
@@ -1,9 +1,22 @@
 var xmlUtil = require('../xmlUtil/xmlUtil')
+var assert = require('assert');
 
-var xmlData = "<xml><ToUserName>Spursyy</ToUserName><FromUserName>WeiChat</FromUserName><CreateTime>123456789</CreateTime><MsgType>event</MsgType><Event>subscribe</Event></xml>"
+var xmlData = "<xml><ToUserName><Name1>Spursyy</Name1> <Name2>YY</Name2></ToUserName><FromUserName><![CDATA[WeiChat]]></FromUserName><CreateTime>123456789</CreateTime><MsgType><![CDATA[event]]></MsgType><Event><![CDATA[subscribe]]></Event></xml>"
 
-xmlUtil.parseXMLAsync(xml2).then(function(data) {
-    console.log(data)
-}).catch(function(err) {
-    console.log(err)
-})
+describe('xmlUtil', function() {
+  describe('formateMessage', function () {
+    it('should return -1 when the value is not present', function (done) {
+
+        xmlUtil.parseXMLAsync(xmlData).then(function(data) {
+            (async function() {
+                var mes = await xmlUtil.formatMessage(data.xml)
+                assert.equal(mes.ToUserName.Name1, "Spursyy");
+                assert.equal(mes.ToUserName.Name2, "YY");
+            })()
+            done()
+            }).catch(function(err) {
+                console.log(err)
+            })
+    });
+  });
+});
