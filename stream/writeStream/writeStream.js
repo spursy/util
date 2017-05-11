3754f7308384def2d704eccd4654bc71c0b39593
spursy <582214816@qq.com>
2017-05-11 21:04:57 +0800

    add write stream.

--- stream/writeStream/writeStream.js
+++ stream/writeStream/writeStream.js
@@ -1,6 +1,13 @@
-var util = require('require');
+var util = require('util');
 var stream = require('stream');
 
 function WriteStream(options) {
-    stream.Writable()
+    stream.Writable.call(this, options);
 }
+util.inherits(WriteStream, stream.Writable); 
+
+WriteStream.prototype._write = function (chunk, encoding, callback) {
+    process.stdout.write("<<" + chunk + ">>" );
+    callback;
+}
+process.stdin.pipe(new WriteStream());
