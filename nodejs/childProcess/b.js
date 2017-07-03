1b44232a8ec73b664a6bf680135cb21db7a2aad7
spursy <582214816@qq.com>
2017-07-03 18:53:27 +0800

    update spawn demo.

--- nodejs/childProcess/b.js
+++ nodejs/childProcess/b.js
@@ -6,7 +6,7 @@ var fpath = path.join(__dirname, './text.txt');
 console.log(`12`);
 fs.open(fpath, 'w', function(err, fd) {
     console.log(fd);
-    while (true) {
+    // while (true) {
         fs.write(fd, process.pid + '\n', function() {});
-    }
+    // }
 })
--- nodejs/childProcess/spawnDemo.js
+++ nodejs/childProcess/spawnDemo.js
@@ -1,10 +1,14 @@
 const spawn = require('child_process').spawn;
 const process = require('process');
 
-const p = spawn('node', ['./b.js']);
-console.log(process.pid, p.pid);
+const p = spawn('node', ['b']);
+// console.log(process.pid, p.pid);
 
-p.stdout.on('data', function (data) {
+p.stdout.on('data',function (data) {
     console.log(`data processed done.`);
     console.log('stdout: ' + data);
 });
+
+p.stderr.on('error', function (data) {
+    console.log(`data`);
+})
