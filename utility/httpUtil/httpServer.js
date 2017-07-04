c94f413351b66bec1fd79de0e7c0d6fcf2151e9e
spursy <582214816@qq.com>
2017-07-04 08:02:09 +0800

    smimulate memory leak.

--- utility/httpUtil/httpServer.js
+++ utility/httpUtil/httpServer.js
@@ -5,7 +5,27 @@ const hostname = '127.0.0.1';
 var server = http.createServer((req, res) => {
     res.statusCode = '200';
     res.setHeader('Content-Type', "text/plain");
-    res.end("Hello Word\n");
+    
+    // for(var i = 0; i <= 100; i ++) {
+    //     var student = {
+    //         name: `I am ${i}`,
+    //         age: `I am ${i} years`
+    //     }
+    //      res.write(`${JSON.stringify(student)}` );
+    // } 
+
+    setInterval(function() {
+        var student = {
+            name: `I am `,
+            age: `I am years`
+        }
+
+        res.write(`${JSON.stringify(student)}` );
+    }, 500)
+    res.write('\n');
+    // res.end(`END` );
+   
+    // res.end('Hello world!');
 });
 
 server.listen(port, hostname, () => {
