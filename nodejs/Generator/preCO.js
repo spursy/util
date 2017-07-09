5744d4b65bf3c15e230063a8b80bb579b781697f
spursy <582214816@qq.com>
2017-07-09 21:09:35 +0800

    add co demo.

---
+++ nodejs/Generator/preCO.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+var fpath1 = path.join(__dirname, './test1.txt');
+var fpath2 = path.join(__dirname, './test2.txt');
+
+function readFileFun(fpath) {
+    return new Promise(function(resolve, reject){
+        fs.readFile(fpath, function(err, result) {
+            if (err) reject(err);
+            else resolve(result);
+        });
+    });
+}
+
+var gen = function *() {
+    //yield 后是一个promise对象
+    var content1 = yield readFileFun(fpath1); 
+    var content2 = yield readFileFun(fpath2);
+    console.log(`${content1}`); 
+    console.log(`${content2}`); 
+}
+var g = gen();
+g.next().value.then((data) => {
+    // 通过next() 函数向generator 函数体传值
+    g.next(data).value.then((data1) => {
+        g.next(data1);
+    })
+})
